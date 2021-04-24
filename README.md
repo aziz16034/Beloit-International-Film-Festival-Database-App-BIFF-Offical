8c844d70317f9761548a101fdcfbb43b48c79588
aziz16034 <67529117+aziz16034@users.noreply.github.com>
2021-04-24 17:47:25 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# DatabaseCapstonewebapp
+# DatabaseCapstonewebappteam
---
+++ users/apps.py
@@ -0,0 +1,8 @@
+from django.apps import AppConfig
+
+
+class UsersConfig(AppConfig):
+    name = 'users'
+
+class UserConfig(AppConfig):
+    name = 'users'
